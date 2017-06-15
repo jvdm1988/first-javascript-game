c454a476f15cac84f388efce004dbff2333a0956
Jessicavandermarel <vandermareljessica@gmail.com>
2017-06-15 10:09:41 -0400

    added code

--- js/game.js
+++ js/game.js
@@ -8,7 +8,6 @@ var startGame = function () {
   if (timeUp) {
     return;
   }
-
   // Function for 30 sec countdown:
   timeUp=setInterval(timer, 1000); //1000 will run it every 1 second
   // no var on timeUp because we want it global
@@ -22,16 +21,46 @@ var startGame = function () {
     if (count <= 0) {
       clearInterval(timeUp);
       timeUp = null;
+      clearInterval(first);
+      clearInterval(second);
+      clearInterval(third);
     }
    }
+
+   // Add 1 rabbit setinterval timer, 2000
+  //  var rabbitOne = setInterval(randomRabbit, 1000);
+    first = setInterval(randomRabbit, 1000);
+    second = setInterval(randomRabbit, 3000);
+    third = setInterval(randomRabbit, 5000);
+
+  //  setTimeout(function(){
+  //    clearInterval(rabbitOne);
+  //  }, 3000);
 };
 
 var endGame = function () {
-  $(".end").click (function () {
   clearInterval(timeUp);
-});
+
+  clearInterval(first);
+  clearInterval(second);
+  clearInterval(third);
 };
 
+//---------------------------------------------------------
+//function to randomly show rabbit:
+var randomRabbit = function () {
+  var row =  Math.floor(Math.random() * 8) + 1;
+  var col = Math.floor(Math.random() * 10) + 1;
+
+  $(".cell-" + row + "-" + col + " > .rabbit1").css ("display", "inline");
+  $(".cell-" + row + "-" + col + " > .grass1").css ("display", "none");
+
+
+setTimeout(function () {
+  $(".cell-" + row + "-" + col + " > .rabbit1").css ("display", "none");
+  $(".cell-" + row + "-" + col + " > .grass1").css ("display", "inline");
+}, 2000);
+};
 //-----------------------------------------------------------
 // function to start game:
 
@@ -43,35 +72,57 @@ $(document).ready(function () {
 // function to end game:
 
   $(".end").click (function () {
-    endGame ();
+    endGame();
   });
-
 });
 
-
-//---------------------------------------------------------
-//function to randomly show rabbit:
-var showRabbit = (function() {
-
-  var row =  $(".cell ").Math.floor(Math.random() * 4) + 1;
-  var cell = $(".cell").Math.floor(Math.random() * 4) + 1;
-
-
-
-
-
-});
-//
+// $(document).ready(function () {
+//   $(".rabbit1").click (function () {
+//     alert ("+1"); // how to create a non pop up alert?
+//   });
 // });
-
-
 //----------------------------------------------------------
 
-//function for userClickCount:
+//function for score:
+var score = 0;
 
+function gameScore() {
+  score=score+1;
+  $("h4").replaceWith("<h4> SCORE: " + score + " </h4>");
+  }
 
+$(".rabbit1").click (function () {
 
+  gameScore();
+});
 
+// functions instructions modal pop up
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
 
 //---------------------------------------------------------
 
